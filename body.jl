79653b936bec50dd59d7af5cbd6b08eaf2cd1353
djuliannader <djuliannader@gmail.com>
2023-04-29 19:41:30 -0400

    it

--- body.jl
+++ body.jl
@@ -26,7 +26,6 @@ function selfconsistent(l,n,b,epsilon1,epsilon2)
 	 it=0
 	 s=[1 for i in 1:(n-1)]
 	 while (sum(s)>epsilon2) && (it<1000)
-	 it=it+1
 	 lold=10
 	 while (abs(lold-lnew)>epsilon1) 
 	  it=it+1
@@ -41,15 +40,13 @@ function selfconsistent(l,n,b,epsilon1,epsilon2)
 	  U=norm.normalizing(Utemp,2*l/n)
 	  lold=lnew
 	  lnew=eigvals(H)[1]
-	  #println("it ",it," lambda:",lnew," lold:",lold," U:", U[trunc(Int,(n-1)/2)]," Uold:",Uold[trunc(Int,(n-1)/2)])
 	  s=[abs(U[i]-Uold[i]) for i in 1:(n-1)]
 	 end
 	 end
 	 if it<999
-	   return [lnew,it,U,1]
-	 end
-	 if it>999
-	 return [lnew,it-1,U,2]
+	  return [lnew,it,U,1]
+	 else 
+	  return [lnew,it,U,2]
 	 end
 	 end 
 
